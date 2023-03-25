0a932bc20e6ed5ae77edcfc71eef57374c26e4ee
Sergey Kirichenko <spkirich@gmail.com>
2023-03-25 19:05:50 +0300

    Sleep until the time

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use chrono::Timelike;
 use clap::Parser;
 
 #[derive(Parser)]
@@ -11,5 +12,37 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-    println!("{}:{}", args.hour, args.minute);
+
+    match duration_until(args.hour, args.minute) {
+        None => {
+            eprintln!("The time is wrong!");
+            std::process::exit(1);
+        },
+
+        Some(duration) => {
+            println!("The time is right!");
+            std::thread::sleep(duration);
+            println!("Now is the time!");
+        },
+    }
+}
+
+fn duration_until(hour: u32, minute: u32) -> Option<std::time::Duration> {
+    let now = chrono::Local::now();
+
+    let mut then = now
+        .with_hour(hour)?
+        .with_minute(minute)?
+        .with_second(0)?
+        .with_nanosecond(0)?;
+
+    if now > then {
+        then += chrono::Duration::days(1);
+    }
+
+    let duration = (then - now)
+        .to_std()
+        .unwrap();
+
+    Some(duration)
 }
