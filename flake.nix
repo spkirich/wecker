65864a47c4d8df0c28f15003c6b6d4a8198e92ec
Sergey Kirichenko <spkirich@gmail.com>
2023-03-25 15:55:29 +0300

    Init Nix flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            rustc
+            rustfmt
+          ];
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+      }
+    );
+}
